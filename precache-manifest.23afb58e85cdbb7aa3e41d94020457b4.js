10968fadf4855fdc89858bef2ec769d8d2c8bca1
kenbme <guilhermekenned1@gmail.com>
2022-09-24 17:43:01 -0300

    Updates

---
+++ precache-manifest.23afb58e85cdbb7aa3e41d94020457b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87535d47daca66b4774b",
+    "url": "/static/css/main.ff94f7d4.chunk.css"
+  },
+  {
+    "revision": "87535d47daca66b4774b",
+    "url": "/static/js/main.19f3ca6d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "ba4b5fcfbfa4223db616d8488d0f4ca1",
+    "url": "/index.html"
+  }
+];
